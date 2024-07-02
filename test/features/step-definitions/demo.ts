774ef084b46399fd96396fb97860f2ba516a4800
jamisaiteja <jamisaiteja666@gmail.com>
2024-07-02 14:40:17 +0530

    add input ele handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -24,3 +24,54 @@ Then(/^URL should match (.*)$/, async function (exceptedURL) {
   let url = await browser.getUrl();
   expect(url).to.equal(exceptedURL);
 });
+
+/**
+ * Web Interactions
+ */
+
+Given(/^A webpage is opened$/,async function(){
+  await browser.url("/inputs")
+  await browser.setTimeout({implicit : 15000, pageLoad:10000});
+  await browser.maximizeWindow();
+})
+
+When(/^Perform webInteractions$/,async function(){
+  /**
+   * 1.Input Box
+   * Actions:
+   * 1.Type into Input Box
+   * 2.Clear the field and Type or just addValue
+   * 3.Click and Type
+   * 4.Slow Typing
+   */
+
+    let stNum = "12345";
+    let ele = await $(`[type=number]`)
+    await ele.click() // click and enter the value
+    //setValue - clear the field before entering
+    //addValue - wil not clear
+    //await ele.addValue("12345"); // can use setValue here
+   // await browser.pause(2000);
+    //clear the value
+    //await ele.setValue("");
+    for(let i=0;i<stNum.length;i++){
+      let charStr = stNum.charAt(i);
+      await browser.pause(1000);
+      await browser.keys(charStr);
+    }
+
+
+    /**
+     * 2.DropDown
+     * 
+     * Actions:
+     * 1.Assert default option is selected
+     * 2. Select By attribute, text, index
+     * 3. Get a list of Options
+     */
+
+    
+    await browser.debug();
+
+
+})
